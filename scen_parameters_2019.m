e05a85a09a7894c5b846559253db9bf30c918b28
Weipengtw <110186375+Weipengtw@users.noreply.github.com>
2023-06-07 10:33:22 +0800

    Delete scen_parameters_2019.m

--- scen_parameters_2019.m
+++
@@ -1,8 +0,0 @@
-first_epoch   = 2007; %staring time
-last_epoch    = 2011; %ending time
-
-time_unit     = 'yr';
-sig_time      = 8;
-observation_unit     = 'mm';
-sparse_types = {};
-X_rescale = {1};
